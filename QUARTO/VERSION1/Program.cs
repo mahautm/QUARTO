9ab3e3f5f0b44d72cb115523f147a05ae60c7bf6
mmahaut <mmahaut@TP304>
2018-12-04 11:06:59 +0100

    Sauvegarde de mi-scéance

--- QUARTO/VERSION1/Program.cs
+++ QUARTO/VERSION1/Program.cs
@@ -12,16 +12,27 @@ namespace VERSION1
         static void Main(string[] args)
             //ecrire qqchose
         {
+            //Initialisation des pièces
+            // Encodage des pièces disponibles seon la convention(0|1) : couleur(bleu|rouge), taille(petit|grand), forme(rond|carré), remplissage(vide|plein)
+            int[] piecesDispo = { 0000, 1000, 0001, 1001, 0010, 1010, 0011, 1011, 0100, 1100, 0101, 1101, 0110, 1101, 0111, 1111};
+            string[] tabAffichePiece = { "      \n\n   XX   \n  X  X  \n   XX  \n      ", "      \n\n   XX   \n  XXXX  \n   XX  \n      ", "      \n\n  XXXX  \n  X  X  \n  XXXX \n      ", "      \n\n  XXXX  \n  XXXX  \n  XXXX \n      ", "      \n   XX   \n  X  X  \n X    X \n  X  X  \n   XX  ", "      \n   XX   \n  XXXX  \n XXXXXX \n  XXXX  \n   XX  ", "      \n XXXXXX \n X    X \n X    X \n X    X \n XXXXXX", "      \n XXXXXX \n XXXXXX \n XXXXXX \n XXXXXX \n XXXXXX", };
 
-            string a = "      \n XXXXXX \n X    X \n X    X \n X    X \n XXXXXX";
-            Console.WriteLine(a[10]);
-            Console.WriteLine("      \n\n  XXXX  \n  X  X  \n  XXXX \n      ");
-            Console.WriteLine("      \n\n   XX   \n  X  X  \n   XX  \n      ");
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("      \n\n   XX   \n  XXXX  \n   XX  \n      ");
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine("      \n XXXXXX \n X    X \n X    X \n X    X \n XXXXXX");
+            //Initialisation du Tableau
 
+
+            //affichage test de toutes les pièces
+
+            for (int i = 0; i < 8; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine(piecesDispo[2 * i]);
+                Console.WriteLine(tabAffichePiece[i]);
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine(piecesDispo[2 * i + 1]);
+                Console.WriteLine(tabAffichePiece[i]);
+
+            }
+          
         }
     }
 }
