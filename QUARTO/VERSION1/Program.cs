2a362fc497129d0bdc80ee3c30be99b452b5c7c9
mmahaut <mmahaut@TP304>
2018-12-04 09:30:58 +0100

    Test

---
+++ QUARTO/VERSION1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VERSION1
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //ecrire qqchose
+        {
+        }
+    }
+}
