efefeeed56dae5be8c21987c162109214a3eec8c
mmahaut <mmahaut@TP304>
2018-12-04 11:48:14 +0100

    Début d'Affichage

---
+++ QUARTO/VERSION1/CoulissesMateo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VERSION1
+{
+    class CoulissesMateo
+    {
+        public void AfficherPlateau(int[][] plat)
+        {
+            for(int ligne = 0; ligne<plat.Length;ligne++)
+            {
+                for (int colonne = 0; colonne < plat[ligne].Length; colonne++)
+                {
+                    for (int sousligne = 0; sousligne <4;sousligne++)
+                    {
+                        if (plat[ligne][colonne] == -1) Console.Write("     ");
+                        else
+                        {
+                            int indice = 0;
+
+                            //Consversion de binaire en décimale pour accéder à l'indice de l'affichage du tableau
+                            int bin = plat[ligne][colonne];
+                            indice +=bin % 10;
+                            indice += (bin - indice) % 100 / 10;
+                            indice += (bin - bin % 10 - ((bin - bin % 10) % 100))%1000/100;
+
+
+                        }
+                    }
+                   
+                }
+
+            }
+        }
+    }
+}
--- QUARTO/VERSION1/Program.cs
+++ QUARTO/VERSION1/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
-
 namespace VERSION1
 {
     class Program
@@ -17,8 +16,8 @@ namespace VERSION1
             int[] piecesDispo = { 0000, 1000, 0001, 1001, 0010, 1010, 0011, 1011, 0100, 1100, 0101, 1101, 0110, 1101, 0111, 1111};
             string[] tabAffichePiece = { "      \n\n   XX   \n  X  X  \n   XX  \n      ", "      \n\n   XX   \n  XXXX  \n   XX  \n      ", "      \n\n  XXXX  \n  X  X  \n  XXXX \n      ", "      \n\n  XXXX  \n  XXXX  \n  XXXX \n      ", "      \n   XX   \n  X  X  \n X    X \n  X  X  \n   XX  ", "      \n   XX   \n  XXXX  \n XXXXXX \n  XXXX  \n   XX  ", "      \n XXXXXX \n X    X \n X    X \n X    X \n XXXXXX", "      \n XXXXXX \n XXXXXX \n XXXXXX \n XXXXXX \n XXXXXX", };
 
-            //Initialisation du Tableau
-
+            //Initialisation du plateau avec ses identifiants, plus manipulable que les string. Nous décidons que -1 représente une case vide.
+            int[][] plateau = { new int[] {-1,-1,-1,-1 }, new int[] { -1, -1, -1, -1 }, new int[] { -1, -1, -1, -1 }, new int[] { -1, -1, -1, -1 } };
 
             //affichage test de toutes les pièces
 
