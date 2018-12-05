8a303fed990d19cb8bc0b21919d32668e436d6f4
BuildTools <unconfigured@null.spigotmc.org>
2018-12-05 13:21:24 +0100

    AfficherTableau et Piocher sont fonctionnels, même si les couleurs sont à retravailler !

--- QUARTO/VERSION1/CoulissesMateo.cs
+++ QUARTO/VERSION1/CoulissesMateo.cs
@@ -20,31 +20,48 @@ namespace VERSION1
             }
             return decimalNumber;
         }
-        public void AfficherPlateau(int[][] plat)
+        public void AfficherPlateau(int[][] plat, string[] affichage)
         {
             for(int ligne = 0; ligne<plat.Length;ligne++)
             {
-                for (int colonne = 0; colonne < plat[ligne].Length; colonne++)
+                //!!virer les lignes et diviser plat[ligne[colonne par 4 (en division entière)
+                for (int sousligne = 0; sousligne <5;sousligne++)
                 {
-                    for (int sousligne = 0; sousligne <4;sousligne++)
+                    for (int colonne = 0; colonne < plat[ligne].Length; colonne++)
                     {
-                        if (plat[ligne][colonne] == -1) Console.Write("     ");
+                        if ((ligne + colonne) % 2 == 0) Console.BackgroundColor = ConsoleColor.Gray;
+                        else Console.BackgroundColor = ConsoleColor.DarkGray;
+                        if (plat[ligne][colonne] == -1) Console.Write("        ");
                         else
                         {
+                            Console.ForegroundColor = ConsoleColor.Black;
+
                             int indice = 0;
 
                             //Consversion de binaire en décimale pour accéder à l'indice de l'affichage du tableau
                             //On enlève la couleur qui sera traitée séparément
-                            indice = TraduireBinVersDec(plat[ligne][colonne] - 1000);
-                            
+                            indice = TraduireBinVersDec(plat[ligne][colonne] % 1000);
+                            if (plat[ligne][colonne] / 1000 == 1) Console.ForegroundColor = ConsoleColor.Yellow;
+                            else Console.ForegroundColor = ConsoleColor.DarkBlue ;
+                            for (int i = sousligne*8; i < (sousligne +1 ) * 8; i++)
+                            {
+                                Console.Write(affichage[indice][i]);
 
-
+                            }
+                           
                         }
                     }
-                   
+                    Console.Write("\n");
                 }
 
             }
+        }
+        public void Piocher(int[] piecesDispo, int pieceChoisie)
+        {
+            if(pieceChoisie/1000 == 1)
+                piecesDispo[2*TraduireBinVersDec(Math.Abs(pieceChoisie - 1000))+1] = -1;
+            else piecesDispo[2 * TraduireBinVersDec(Math.Abs(pieceChoisie))] = -1;
+
         }
     }
 }
--- QUARTO/VERSION1/Program.cs
+++ QUARTO/VERSION1/Program.cs
@@ -13,29 +13,51 @@ namespace VERSION1
         {
             //Initialisation des pièces
             // Encodage des pièces disponibles seon la convention(0|1) : couleur(bleu|rouge), taille(petit|grand), forme(rond|carré), remplissage(vide|plein)
-            int[] piecesDispo = { 0000, 1000, 0001, 1001, 0010, 1010, 0011, 1011, 0100, 1100, 0101, 1101, 0110, 1101, 0111, 1111};
-            string[] tabAffichePiece = { "      \n\n   XX   \n  X  X  \n   XX  \n      ", "      \n\n   XX   \n  XXXX  \n   XX  \n      ", "      \n\n  XXXX  \n  X  X  \n  XXXX \n      ", "      \n\n  XXXX  \n  XXXX  \n  XXXX \n      ", "      \n   XX   \n  X  X  \n X    X \n  X  X  \n   XX  ", "      \n   XX   \n  XXXX  \n XXXXXX \n  XXXX  \n   XX  ", "      \n XXXXXX \n X    X \n X    X \n X    X \n XXXXXX", "      \n XXXXXX \n XXXXXX \n XXXXXX \n XXXXXX \n XXXXXX", };
+            int[] pieceDispo = { 0000, 1000, 0001, 1001, 0010, 1010, 0011, 1011, 0100, 1100, 0101, 1101, 0110, 1101, 0111, 1111};
+            
+            //l'ordre choisi est le même que dans la pioche, à la différence prêt que les couleures ne sont pas représentées.
+            //ie : Pour chaque string dans le tableau affichage, il y a deux pièces dans la pioche.
+            string[] tabAffichePiece = {    "           XX     X  X     XX           ",
+                                            "           XX     XXXX     XX           ",
+                                            "          XXXX    X  X    XXXX          ",
+                                            "          XXXX    XXXX    XXXX          ",
+                                            "   XX     X  X   X    X   X  X     XX   ",
+                                            "   XX     XXXX   XXXXXX   XXXX     XX   ",
+                                            " XXXXXX  X    X  X    X  X    X  XXXXXX ",
+                                            " XXXXXX  XXXXXX  XXXXXX  XXXXXX  XXXXXX " };
 
             //Initialisation du plateau avec ses identifiants, plus manipulable que les string. Nous décidons que -1 représente une case vide.
             int[][] plateau = { new int[] {-1,-1,-1,-1 }, new int[] { -1, -1, -1, -1 }, new int[] { -1, -1, -1, -1 }, new int[] { -1, -1, -1, -1 } };
 
             //affichage test de toutes les pièces
 
-            for (int i = 0; i < 8; i++)
-            {
-                Console.ForegroundColor = ConsoleColor.DarkCyan;
-                Console.WriteLine(piecesDispo[2 * i]);
-                Console.WriteLine(tabAffichePiece[i]);
-                Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.WriteLine(piecesDispo[2 * i + 1]);
-                Console.WriteLine(tabAffichePiece[i]);
+            ////for (int i = 0; i < 8; i++)
+            ////{
+            ////    Console.ForegroundColor = ConsoleColor.DarkCyan;
+            ////    Console.WriteLine(pioche[2 * i]);
+            ////    Console.WriteLine(tabAffichePiece[i]);
+            ////    Console.ForegroundColor = ConsoleColor.Magenta;
+            ////    Console.WriteLine(pioche[2 * i + 1]);
+            ////    Console.WriteLine(tabAffichePiece[i]);
 
-            }
+            //}
+            Console.WriteLine("A");
+            Console.WriteLine("        \n        \n   XX   \n  X  X  \n   XX   \n        \n");
 
             //test Convertisseur binaire
             CoulissesMateo cm = new CoulissesMateo();
-            int a = cm.TraduireBinVersDec(101);
-            Console.WriteLine(a);
+            //plateau test
+            int[][] plateautest = { new int[] { 0000, 1001, 1001, 0011 }, new int[] { 0111, 0101, -1, 0110 }, new int[] { -1, 0110, 0111, 0010 }, new int[] { -1, -1, -1, 0011 } };
+
+            //cm.Piocher(pieceDispo, 1001);
+            //cm.Piocher(pieceDispo, 0101);
+            //for (int i = 0; i < pieceDispo.Length; i++)
+            //{
+            //    Console.WriteLine(pieceDispo[i]);
+            //}
+
+            cm.AfficherPlateau(plateautest, tabAffichePiece);
+
         }
     }
 }
